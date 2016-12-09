39482fb20400ddff327cbf2a37cf5df4e5a99493
John Carpenter <jcarpent@gmail.com>
2016-12-08 21:34:24 -0700

    cleanup login page

--- src/app/auth/components/sign-in.ts
+++ src/app/auth/components/sign-in.ts
@@ -4,20 +4,10 @@ import { AuthService } from '../services/auth-service';
 
 
 @Component({
-  styles: [
+  styleUrls: [
     './sign-in.scss'
   ],
-  template: `
-    <div class="g-row sign-in">
-      <div class="g-col">
-        <h1 class="sign-in__heading">Sign in</h1>
-        <button class="sign-in__button" (click)="signInAnonymously()" type="button">Anonymously</button>
-        <button class="sign-in__button" (click)="signInWithGithub()" type="button">GitHub</button>
-        <button class="sign-in__button" (click)="signInWithGoogle()" type="button">Google</button>
-        <button class="sign-in__button" (click)="signInWithTwitter()" type="button">Twitter</button>
-      </div>
-    </div>
-  `
+  templateUrl: './sign-in.html',
 })
 
 export class SignInComponent {
